e1be3410ee33dbbae8affc9e4a729b492ee1fb22
Dániel Bene <danielbene@protonmail.com>
2021-04-10 19:52:50 +0200

    Add gui improvements

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import tkinter as tk
 
 window = tk.Tk()
 
+### FRAMES
 main_frame = tk.Frame(
     master=window,
     relief=tk.RAISED,
+    borderwidth=1
+)
+
+button_frame = tk.Frame(
+    master=main_frame,
     borderwidth=1
 )
 
@@ -14,19 +20,44 @@ entry_key_frame = tk.Frame(
     borderwidth=1
 )
 
-main_frame.pack()
-entry_key_frame.pack()
+### BUTTONS
+button_encrypt = tk.Button(
+    master=button_frame,
+    text="Encrypt",
+    width=10,
+    height=3
+)
+
+button_compare = tk.Button(
+    master=button_frame,
+    text="Compare",
+    width=10,
+    height=3
+)
+
+button_decrypt = tk.Button(
+    master=button_frame,
+    text="Decrypt",
+    width=10,
+    height=3
+)
+
+### BUILD
+main_frame.pack(fill=tk.BOTH)
+entry_key_frame.pack(fill=tk.BOTH)
 
 text_input = tk.Text(master=main_frame, width=50, height=15)
-text_input.pack(side=tk.LEFT)
+text_input.pack(fill=tk.BOTH, side=tk.LEFT)
 
-label = tk.Label(master=main_frame, text="kolbasz")
-label.pack(side=tk.LEFT)
+button_frame.pack(side=tk.LEFT)
+button_encrypt.pack(side=tk.TOP)
+button_compare.pack(side=tk.TOP)
+button_decrypt.pack(side=tk.TOP)
 
 text_output = tk.Text(master=main_frame, width=50, height=15)
-text_output.pack(side=tk.LEFT)
+text_output.pack(fill=tk.BOTH, side=tk.LEFT)
 
 entry_key = tk.Entry(master=entry_key_frame)
-entry_key.pack()
+entry_key.pack(fill=tk.BOTH)
 
 window.mainloop()
